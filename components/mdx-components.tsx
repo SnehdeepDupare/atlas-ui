81f8d08ede9846564a67e6e98f2ec564ad4956cc
Snehdeep <duparesnehdeep@gmail.com>
2025-02-02 23:13:03 +0530

    decrease height for pre tag

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -150,7 +150,7 @@ const components = {
       <div className="relative">
         <pre
           className={cn(
-            "mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-xl bg-zinc-950 py-4 dark:bg-zinc-900",
+            "mb-4 mt-6 max-h-[400px] overflow-x-auto rounded-xl bg-zinc-950 py-4 dark:bg-zinc-900",
             className
           )}
           {...props}
