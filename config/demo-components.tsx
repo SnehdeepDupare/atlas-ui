e7ba755f379b9f9d8f2a2285e2759078093b4725
Snehdeep <duparesnehdeep@gmail.com>
2025-02-02 23:09:15 +0530

    add animated-mobile-navbar

--- config/demo-components.tsx
+++ config/demo-components.tsx
@@ -1,22 +1,22 @@
 import { lazy } from "react";
 
 export const demoComponents: Record<string, any> = {
-  // "animated-mobile-navbar-demo": {
-  //   component: lazy(async () => ({
-  //     default: (
-  //       await import(
-  //         "components/demo-components/react/animated-mobile-navbar-demo"
-  //       )
-  //     ).AnimatedMobileNavbarDemo,
-  //   })),
-  //   path: "components/demo-components/react/animated-mobile-navbar-demo.tsx",
-  // },
-  // "animated-mobile-navbar": {
-  //   component: lazy(async () => ({
-  //     default: (
-  //       await import("components/atlas_ui/(react)/animated-mobile-navbar")
-  //     ).AnimatedMobileNavbar,
-  //   })),
-  //   path: "components/atlas_ui/(react)/animated-mobile-navbar.tsx",
-  // },
+  "animated-mobile-navbar-demo": {
+    component: lazy(async () => ({
+      default: (
+        await import(
+          "components/demo-components/react/animated-mobile-navbar-demo"
+        )
+      ).AnimatedMobileNavbarDemo,
+    })),
+    path: "components/demo-components/react/animated-mobile-navbar-demo.tsx",
+  },
+  "animated-mobile-navbar": {
+    component: lazy(async () => ({
+      default: (
+        await import("components/atlas_ui/(react)/animated-mobile-navbar")
+      ).AnimatedMobileNavbar,
+    })),
+    path: "components/atlas_ui/(react)/animated-mobile-navbar.tsx",
+  },
 };
