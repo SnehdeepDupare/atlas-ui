d3868160b2c1a7b7959b497495b8c5769c9160fb
Snehdeep <duparesnehdeep@gmail.com>
2025-02-02 23:38:54 +0530

    add table of content for docs pages

--- app/(docs)/docs/[[...slug]]/page.tsx
+++ app/(docs)/docs/[[...slug]]/page.tsx
@@ -6,6 +6,8 @@ import "@/styles/mdx.css";
 import { allDocs } from "contentlayer/generated";
 import { Mdx } from "@/components/mdx-components";
 import { DocsPager } from "@/components/docs-pagination";
+import { DashboardTableOfContents } from "@/components/toc";
+import { getTableOfContents } from "@/lib/toc";
 
 interface DocPageProps {
   params: {
@@ -25,9 +27,11 @@ async function getDocFromParams({ params }: DocPageProps) {
 const DocPage = async ({ params }: DocPageProps) => {
   const doc = await getDocFromParams({ params });
 
+  const toc = await getTableOfContents(doc.body.raw);
+
   return (
-    <main className="relative py-6 lg:gap-10 lg:py-8 ">
-      <div className="mx-auto w-full max-w-4xl">
+    <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <div className="mx-auto w-full min-w-0 max-w-4xl">
         <div className="flex items-center space-x-2 mb-4">
           <p className="text-muted-foreground">Docs</p>
           <ChevronRight className="h-4 w-4" />
@@ -44,6 +48,13 @@ const DocPage = async ({ params }: DocPageProps) => {
         </div>
 
         <DocsPager doc={doc} />
+      </div>
+      <div className="hidden text-sm xl:block">
+        <div className="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] pt-4">
+          <div className="no-scrollbar h-full overflow-auto pb-10">
+            {doc.toc && <DashboardTableOfContents toc={toc} />}
+          </div>
+        </div>
       </div>
     </main>
   );
