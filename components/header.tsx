7e80ad9c4d40b125d428805eb4171c9a28622f69
Snehdeep <duparesnehdeep@gmail.com>
2025-03-08 17:39:33 +0530

    changes according to docs grid pattern

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
     }
   };
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 shadow-md dark:shadow-primary-foreground">
+    <header className="sticky top-0 z-50 w-full border-b  backdrop-blur dark:backdrop-blur-sm   shadow-md dark:shadow-primary-foreground">
       <div className="container-wrapper">
         <div className="container flex h-14 items-center">
           <div className="md:hidden">
