b31bd8b841ed9b97cc6af5b23c99a0eca3403075
Snehdeep <duparesnehdeep@gmail.com>
2025-05-12 22:52:11 +0530

    update styles and links

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,14 +4,15 @@ import { siteConfig } from "@/config/site";
 
 export const Footer = () => {
   return (
-    <footer className="border-t py-6 md:px-8 md:py-0">
+    <footer className="border-t py-6 md:py-0">
       <div className="container-wrapper">
         <div className="container py-4">
           <div className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
             Developed by{" "}
             <Link
-              href={siteConfig.links.portfolio}
+              href={siteConfig.links.twitter}
               target="_blank"
+              rel="noreferrer noopenner"
               className="font-medium underline underline-offset-4 hover:text-primary"
             >
               Snehdeep Dupare
@@ -20,7 +21,7 @@ export const Footer = () => {
             <Link
               href={siteConfig.links.github}
               target="_blank"
-              rel="noreferrer"
+              rel="noreferrer noopener"
               className="font-medium underline underline-offset-4 hover:text-primary"
             >
               Github
