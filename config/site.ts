50aa9e2c389f3d8f5559410ff582bb65ce1d709b
Snehdeep <duparesnehdeep@gmail.com>
2025-05-12 23:37:26 +0530

    add production url

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Atlas UI",
-  url: "http://localhost:3000", // TODO: Update this to your production URL
-  ogImage: "http://localhost:3000/og.png", // TODO: Update this to your production URL
+  url: "https://atlasui.vercel.app",
+  ogImage: "https://atlasui.vercel.app/og.png",
   description:
     "An open-source library of prebuilt components, crafted with TypeScript and Tailwind CSS. Powered by motion for React and Next.js, with seamless options for Vanilla JS and CSS. Built to save time, inspire creativity, and elevate your UI.",
   links: {
