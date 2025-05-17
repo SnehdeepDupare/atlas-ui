b11dd0324086415a63cd0e337152b4d94e560312
Snehdeep <duparesnehdeep@gmail.com>
2025-05-17 21:24:26 +0530

    optimize video-showcase component

--- components/video-showcase.tsx
+++ components/video-showcase.tsx
@@ -2,195 +2,201 @@
 
 import { motion } from "motion/react";
 import { Video } from "./video";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { cn } from "@/lib/utils";
 
 export const VideoShowcase = () => {
-  const containerVariants = {
-    hidden: {},
-    show: {
-      transition: {
-        staggerChildren: 0.15,
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: {
-      opacity: 0,
-      scale: 0,
-      filter: "blur(8px)",
-    },
-    show: {
-      opacity: 1,
-      scale: 1,
-      filter: "blur(0px)",
-      transition: {
-        duration: 0.4,
-        ease: "easeIn",
-      },
-    },
-  };
+  const isMobile = useIsMobile();
 
   return (
-    <>
-      <section className="relative hidden md:flex w-full max-w-screen-2xl mx-auto min-h-[800px] items-center justify-center mt-10 px-4 sm:px-6 lg:px-8">
-        {/* Top Left */}
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8, rotate: 0, x: 200, y: 150 }}
-          whileInView={{ opacity: 1, scale: 1, rotate: -25, x: 0, y: 0 }}
-          whileHover={{ scale: 1.05, rotate: -20 }}
-          whileTap={{ scale: 0.95, rotate: -25 }}
-          transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
-          viewport={{ once: true, amount: 0.2 }}
-          className="absolute top-4 sm:top-10 left-4 sm:left-[8%] z-10 w-[40vw] sm:w-[20rem] rounded-xl overflow-hidden border-2 border-neutral-800"
-        >
-          <Video
-            href="/docs/components/parallax-sections"
-            src="/assets/videos/parallax-section-demo.mp4"
-          />
-        </motion.div>
-
-        {/* Top Right */}
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8, rotate: 0, x: -200, y: 150 }}
-          whileInView={{ opacity: 1, scale: 1, rotate: 15, x: 0, y: 0 }}
-          transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
-          whileHover={{ scale: 1.05, rotate: 10 }}
-          whileTap={{ scale: 0.95, rotate: 15 }}
-          viewport={{ once: true, amount: 0.2 }}
-          className="absolute top-6 sm:top-12 right-4 sm:right-[15%] z-10 w-[40vw] sm:w-[200px] rounded-xl border-2 border-neutral-800 overflow-hidden"
-        >
-          <Video
-            href="/docs/components/hacker-text"
-            src="/assets/videos/hacker-text-demo.mp4"
-          />
-        </motion.div>
-
-        {/* Project Gallery Video - Center */}
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8, rotate: 0 }}
-          whileInView={{ opacity: 1, scale: 1, rotate: 5 }}
-          whileHover={{ scale: 1.04, rotate: 3 }}
-          whileTap={{ scale: 0.97, rotate: 5 }}
-          transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
-          viewport={{ once: true, amount: 0.2 }}
-          className="absolute z-20 w-full max-w-lg sm:max-w-xl md:max-w-3xl h-[60vw] sm:h-[30rem] top-28"
-        >
-          <div className="border-2 border-neutral-800 rounded-xl overflow-hidden h-full w-full">
-            <Video
-              src="/assets/videos/project-gallery-demo.mp4"
-              href="/docs/components/project-gallery"
-              className="w-full h-full rounded-lg object-cover"
-              height="100%"
-            />
-          </div>
-        </motion.div>
-
-        {/* Bottom Left */}
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8, rotate: 0, x: 200, y: -150 }}
-          whileInView={{ opacity: 1, scale: 1, rotate: -12, x: 0, y: 0 }}
-          transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
-          whileHover={{ scale: 1.05, rotate: -8 }}
-          whileTap={{ scale: 0.95, rotate: -12 }}
-          viewport={{ once: true }}
-          className="absolute bottom-20 left-4 sm:left-[10%] z-10 w-[40vw] sm:w-[20rem]"
-        >
-          <div className="rounded-xl border-2 border-neutral-800 overflow-hidden">
-            <Video
-              href="/docs/components/blend-mouse-trailer"
-              src="/assets/videos/blend-mouse-trailer-demo.mp4"
-            />
-          </div>
-        </motion.div>
-
-        {/* Bottom Right */}
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8, rotate: 0, x: -200, y: -150 }}
-          whileInView={{ opacity: 1, scale: 1, rotate: -12, x: 0, y: 0 }}
-          whileHover={{ scale: 1.05, rotate: -8 }}
-          whileTap={{ scale: 0.95, rotate: -12 }}
-          transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
-          viewport={{ once: true }}
-          className="absolute bottom-24 right-4 sm:right-[10%] z-10 w-[45vw] sm:w-[250px]"
-        >
-          <div className="rounded-xl border-2 border-neutral-800 overflow-hidden">
-            <Video
-              href="/docs/components/dot-and-circle-mouse-trailer"
-              src="/assets/videos/dot-and-circle-mouse-trailer-demo.mp4"
-            />
-          </div>
-        </motion.div>
-      </section>
-
-      {/* For Mobile Devices */}
-      <motion.section
-        variants={containerVariants}
-        initial="hidden"
-        whileInView="show"
+    <motion.section
+      initial={
+        isMobile
+          ? {
+              opacity: 0,
+              filter: "blur(8px)",
+            }
+          : {}
+      }
+      whileInView={
+        isMobile
+          ? {
+              opacity: 1,
+              filter: "blur(0px)",
+            }
+          : {}
+      }
+      viewport={{ once: true }}
+      transition={{ duration: 0.4, ease: [0.16, 1, 0.3, 1] }}
+      className={cn(
+        "relative w-full max-w-screen-2xl mx-auto mt-10 px-4 sm:px-6 lg:px-8",
+        isMobile
+          ? "grid grid-cols-2 gap-4"
+          : "flex items-center justify-center min-h-[800px]"
+      )}
+    >
+      {/* Top Left */}
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.8,
+          rotate: 0,
+          x: isMobile ? 0 : 200,
+          y: isMobile ? 0 : 150,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+          rotate: isMobile ? 0 : -25,
+          x: 0,
+          y: 0,
+        }}
+        whileHover={!isMobile ? { scale: 1.05, rotate: -20 } : {}}
+        whileTap={{ scale: 0.95, rotate: isMobile ? 0 : -25 }}
+        transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
         viewport={{ once: true, amount: 0.2 }}
-        className="mt-10 px-4 sm:px-6 lg:px-8 grid grid-cols-1 gap-4 md:hidden"
+        className={cn(
+          "rounded-xl overflow-hidden border-2 border-neutral-800",
+          isMobile
+            ? ""
+            : "absolute top-4 sm:top-10 left-4 sm:left-[8%] z-10 w-[40vw] sm:w-[20rem]"
+        )}
       >
-        <div className="grid grid-cols-2 gap-4">
-          <motion.div
-            variants={itemVariants}
-            whileTap={{ scale: 0.95 }}
-            className="rounded-xl border-2 border-neutral-800 overflow-hidden"
-          >
-            <Video
-              href="/docs/components/parallax-sections"
-              src="/assets/videos/parallax-section-demo.mp4"
-            />
-          </motion.div>
+        <Video
+          href="/docs/components/parallax-sections"
+          src="/assets/videos/parallax-section-demo.mp4"
+          className={isMobile ? "w-full h-full object-cover" : undefined}
+        />
+      </motion.div>
 
-          <motion.div
-            variants={itemVariants}
-            whileTap={{ scale: 0.95 }}
-            className="rounded-xl border-2 border-neutral-800 overflow-hidden"
-          >
-            <Video
-              href="/docs/components/hacker-text"
-              src="/assets/videos/hacker-text-demo.mp4"
-            />
-          </motion.div>
-        </div>
+      {/* Top Right */}
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.8,
+          rotate: 0,
+          x: isMobile ? 0 : -200,
+          y: isMobile ? 0 : 150,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+          rotate: isMobile ? 0 : 15,
+          x: 0,
+          y: 0,
+        }}
+        whileHover={!isMobile ? { scale: 1.05, rotate: 10 } : {}}
+        whileTap={{ scale: 0.95, rotate: isMobile ? 0 : 15 }}
+        transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
+        viewport={{ once: true, amount: 0.2 }}
+        className={cn(
+          "rounded-xl overflow-hidden border-2 border-neutral-800",
+          isMobile
+            ? ""
+            : "absolute top-6 sm:top-12 right-4 sm:right-[15%] z-10 w-[40vw] sm:w-[200px]"
+        )}
+      >
+        <Video
+          href="/docs/components/hacker-text"
+          src="/assets/videos/hacker-text-demo.mp4"
+          className={isMobile ? "w-full h-full object-cover" : undefined}
+        />
+      </motion.div>
 
-        <motion.div
-          variants={itemVariants}
-          whileTap={{ scale: 0.95 }}
-          className="border-2 border-neutral-800 rounded-xl overflow-hidden h-full w-full"
-        >
-          <Video
-            src="/assets/videos/project-gallery-demo.mp4"
-            href="/docs/components/project-gallery"
-            className="w-full h-full object-cover rounded-[4px]"
-            height="100%"
-          />
-        </motion.div>
+      {/* Center */}
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+          rotate: isMobile ? 0 : 5,
+        }}
+        whileHover={!isMobile ? { scale: 1.05, rotate: 2.5 } : {}}
+        whileTap={{ scale: 0.95, rotate: isMobile ? 0 : 5 }}
+        transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
+        viewport={{ once: true, amount: 0.2 }}
+        className={cn(
+          "rounded-xl overflow-hidden border-2 border-neutral-800 z-20",
+          isMobile
+            ? "col-span-2"
+            : "absolute top-28 w-full max-w-lg sm:max-w-xl md:max-w-3xl h-[60vh] sm:h-[30rem]"
+        )}
+      >
+        <Video
+          href="/docs/components/project-gallery"
+          src="/assets/videos/project-gallery-demo.mp4"
+          className="w-full h-full object-cover rounded-lg"
+          height="100%"
+        />
+      </motion.div>
 
-        <div className="grid grid-cols-2 gap-4">
-          <motion.div
-            variants={itemVariants}
-            whileTap={{ scale: 0.95 }}
-            className="rounded-xl border-2 border-neutral-800 overflow-hidden"
-          >
-            <Video
-              href="/docs/components/blend-mouse-trailer"
-              src="/assets/videos/blend-mouse-trailer-demo.mp4"
-            />
-          </motion.div>
+      {/* Bottom Left */}
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.8,
+          rotate: 0,
+          x: isMobile ? 0 : 200,
+          y: isMobile ? 0 : -150,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+          rotate: isMobile ? 0 : -12,
+          x: 0,
+          y: 0,
+        }}
+        whileHover={!isMobile ? { scale: 1.05, rotate: -8 } : {}}
+        whileTap={{ scale: 0.95, rotate: isMobile ? 0 : -12 }}
+        transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
+        viewport={{ once: true, amount: 0.2 }}
+        className={cn(
+          "rounded-xl overflow-hidden border-2 border-neutral-800",
+          isMobile
+            ? ""
+            : "absolute bottom-20 left-4 sm:left-[10%] z-10 w-[40vw] sm:w-[20rem]"
+        )}
+      >
+        <Video
+          href="/docs/components/blend-mouse-trailer"
+          src="/assets/videos/blend-mouse-trailer-demo.mp4"
+          className={isMobile ? "w-full h-full object-cover" : undefined}
+        />
+      </motion.div>
 
-          <motion.div
-            variants={itemVariants}
-            whileTap={{ scale: 0.95 }}
-            className="rounded-xl border-2 border-neutral-800 overflow-hidden"
-          >
-            <Video
-              href="/docs/components/dot-and-circle-mouse-trailer"
-              src="/assets/videos/dot-and-circle-mouse-trailer-demo.mp4"
-            />
-          </motion.div>
-        </div>
-      </motion.section>
-    </>
+      {/* Bottom Right */}
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.8,
+          rotate: 0,
+          x: isMobile ? 0 : -200,
+          y: isMobile ? 0 : -150,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+          rotate: isMobile ? 0 : -12,
+          x: 0,
+          y: 0,
+        }}
+        whileHover={!isMobile ? { scale: 1.05, rotate: -8 } : {}}
+        whileTap={{ scale: 0.95, rotate: isMobile ? 0 : -12 }}
+        transition={{ duration: 0.75, ease: [0, 0.55, 0.45, 1] }}
+        viewport={{ once: true, amount: 0.2 }}
+        className={cn(
+          "rounded-xl overflow-hidden border-2 border-neutral-800",
+          isMobile
+            ? ""
+            : "absolute bottom-24 right-4 sm:right-[10%] z-10 w-[45vw] sm:w-[250px]"
+        )}
+      >
+        <Video
+          href="/docs/components/dot-and-circle-mouse-trailer"
+          src="/assets/videos/dot-and-circle-mouse-trailer-demo.mp4"
+          className={isMobile ? "w-full h-full object-cover" : undefined}
+        />
+      </motion.div>
+    </motion.section>
   );
 };
