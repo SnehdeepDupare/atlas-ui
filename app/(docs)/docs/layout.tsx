5e076fa00258feb424b37de4d177f5525eaff835
Snehdeep <duparesnehdeep@gmail.com>
2025-05-18 11:00:15 +0530

    fix: adjust height and scroll for sidebar

--- app/(docs)/docs/layout.tsx
+++ app/(docs)/docs/layout.tsx
@@ -10,9 +10,11 @@ export default function DocsLayout({
   return (
     <div className="container-wrapper">
       <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-        <aside className="fixed top-14 z-30 hidden h-[calc(100vh-1.5rem)] w-full shrink-0 border-r md:sticky md:block ">
-          <ScrollArea className="h-full pr-4 py-6 lg:py-8">
-            <DocsNav config={docsConfig} />
+        <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 border-r md:sticky md:block ">
+          <ScrollArea className="h-full">
+            <div className="h-full pr-4 py-6 lg:py-8 overflow-auto">
+              <DocsNav config={docsConfig} />
+            </div>
           </ScrollArea>
         </aside>
         {children}
