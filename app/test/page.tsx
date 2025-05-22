50ab48dff1c3bb280f53ffdde8374809883e9d98
Snehdeep <duparesnehdeep@gmail.com>
2025-05-22 23:21:49 +0530

    ignore test folder

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProjectGalleryDemo } from "@/components/demo-components/react/project-gallery-demo";
-
-const TestPage = () => {
-  return <ProjectGalleryDemo />;
-};
-
-export default TestPage;
