25f4402c9d00cb36cf4e7368c5c6d52eef48a403
Snehdeep <duparesnehdeep@gmail.com>
2025-05-31 19:39:37 +0530

    fix: update text in for demo component

--- components/demo-components/react/hacker-text-demo.tsx
+++ components/demo-components/react/hacker-text-demo.tsx
@@ -1,5 +1,5 @@
 import { HackerText } from "@/components/atlas_ui/(react)/hacker-text";
 
 export const HackerTextDemo = () => {
-  return <HackerText text="Atlas UI" />;
+  return <HackerText text="Hover Me!" />;
 };
