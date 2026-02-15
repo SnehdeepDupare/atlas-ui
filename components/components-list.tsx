63040a19b520172462b8d32961de20ac0ac32172
Snehdeep <duparesnehdeep@gmail.com>
2026-02-15 13:19:04 +0530

    refactor ComponentsList to use docsConfig for component retrieval

--- components/components-list.tsx
+++ components/components-list.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 
-import { allDocs } from "contentlayer/generated";
+import { docsConfig } from "@/config/docs";
 
 export const ComponentsList = () => {
-  const components = allDocs.filter((doc) =>
-    doc.slugAsParams?.startsWith("components/")
+  const components = docsConfig.sidebarNav.flatMap(
+    (item) =>
+      item.items?.filter(
+        (subItem) =>
+          subItem.href?.startsWith("/docs/components") &&
+          subItem.href !== "/docs/components"
+      ) || []
   );
 
   if (components.length === 0) {
@@ -13,13 +18,21 @@ export const ComponentsList = () => {
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-x-6 lg:gap-y-6">
-      {components.map((component) => (
+      {components.map((component, index) => (
         <Link
-          key={component._id}
-          href={component.slug}
-          className="text-base font-medium underline-offset-4 hover:underline"
+          key={index}
+          href={component.href!}
+          className="group text-base font-medium"
         >
-          {component.title}
+          <span className="underline-offset-4 group-hover:underline">
+            {component.title}
+          </span>
+
+          {component.label && (
+            <span className="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none font-normal text-[#000000] no-underline group-hover:no-underline">
+              {component.label}
+            </span>
+          )}
         </Link>
       ))}
     </div>
