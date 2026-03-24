93807d0f77cafa0a6399133ad3b3dcf30cd23fac
Snehdeep <duparesnehdeep@gmail.com>
2026-03-24 17:12:34 +0530

    chore: remove unused files and update project-gallery-demo styles

--- app/preview/project-gallery/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProjectGalleryDemo } from "@/registry/react/examples/project-gallery-demo";
-
-const ProjectGalleryPreviewPage = () => {
-  return <ProjectGalleryDemo />;
-};
-
-export default ProjectGalleryPreviewPage;
