eabf7590da93667fe31c7dc10109c699bdf00859
Snehdeep <duparesnehdeep@gmail.com>
2026-09-14 17:15:22 +0530

    feat: migrate to cn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
 import { allDocs } from "@/.contentlayer/generated";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export { cn } from "cn";
 
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
